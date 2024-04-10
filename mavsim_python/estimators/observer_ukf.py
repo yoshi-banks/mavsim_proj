3565a4442b2a5cb74cbb4b87126d059f60c127b7
Joshua Miller <jmyoshibanks@gmail.com>
2024-04-09 20:04:31 -0700

    create ukf observer

--- mavsim_python/estimators/observer_ukf.py
+++ mavsim_python/estimators/observer_ukf.py
@@ -1,14 +1,122 @@
 import numpy as np
-from estimators.observer_filter_lib import ObserverFilterLib
+from estimators.observer_base import ObserverBase
 from estimators.filter_library.src.discrete_unscented_kalman_filter import DiscreteUnscentedKalmanFilter
-import parameters.estimation_parameters1 as EST
+# import parameters.estimation_pararameters_ukf as EST
+import parameters.estimation_parameters_ekf as EST
 from message_types.msg_sensors import MsgSensors
+from message_types.msg_state import MsgState
 
-class ObserverEKF(ObserverFilterLib):
+class ObserverUKF(ObserverBase):
 
     def __init__(self, t0: float, initial_measurements: MsgSensors=MsgSensors()):
         super().__init__(initial_measurements)
 
-        self.attitude_filter = 
+        self.t_minus = t0
 
-        self.position_filter = 
+        self.attitude_filter = DiscreteUnscentedKalmanFilter(EST.xhat0_attitude, EST.P0_attitude)
+
+        self.position_filter = DiscreteUnscentedKalmanFilter(EST.xhat0_position, EST.P0_position)
+
+        self.attitudeParams = {}
+        self.attitudeDynParams = {
+            'Afunc': lambda x, u, t, params: self.Afunc_attitude(x, u),
+            'Lfunc': lambda x, u, t, params: self.Lfunc_attitude(x, u),
+            'Gfunc': lambda x, u, t, params: self.Gfunc_attitude(x, u),
+            'fdis': lambda x, u, t, params: self.fdis_attitude(x, u, t, params),
+            'Q': EST.Q_attitude,
+            'Qu': EST.Qu_attitude,
+        }
+        self.accelMeasParams = {
+            'h': lambda x, u, t, params: self.h_accel(x, u),
+            'Hfunc': lambda x, u, t, params: self.Hfunc_accel(x, u),
+            'Mfunc': lambda x, u, t, params: self.Mfunc_accel(x, u),
+            'R': EST.R_accel,
+            'gate_threshold': EST.accel_gate_threshold,
+        }
+
+        self.positionParams = {}
+        self.positionDynParams = {
+            'Afunc': lambda x, u, t, params: self.Afunc_smooth(x, u),
+            'Lfunc': lambda x, u, t, params: self.Lfunc_smooth(x, u),
+            'Gfunc': lambda x, u, t, params: self.Gfunc_smooth(x, u),
+            'f': lambda x, u, t, params: self.f_smooth(x, u),
+            'fdis': lambda x, u, t, params: self.fdis_smooth(x, u, t, params),
+            'Q': EST.Q_position,
+            'Qu': EST.Qu_position,
+        }
+        self.pseudoMeasParams = {
+            'h': lambda x, u, t, params: self.h_pseudo(x, u),
+            'Hfunc': lambda x, u, t, params: self.Hfunc_pseudo(x, u),
+            'Mfunc': lambda x, u, t, params: self.Mfunc_pseudo(x, u),
+            'R': EST.R_pseudo,
+            'gate_threshold': EST.pseudo_gate_threshold,
+        }
+        self.gpsMeasParams = {
+            'h': lambda x, u, t, params: self.h_gps(x, u),
+            'Hfunc': lambda x, u, t, params: self.Hfunc_gps(x, u),
+            'Mfunc': lambda x, u, t, params: self.Mfunc_gps(x, u),
+            'R': EST.R_gps,
+            'gate_threshold': None,
+        }
+
+    def update(self, measurement: MsgSensors, t) -> MsgState:
+        dt = t - self.t_minus
+        self.attitudeParams['dt'] = dt
+        self.positionParams['dt'] = dt
+        self.estimated_state = super().update(measurement, t)
+        self.t_minus = t
+        return self.estimated_state
+        
+    def propagate_attitude_model(self, u: np.ndarray, t) -> tuple[np.ndarray, np.ndarray]:
+        '''
+        Args:
+            u: p, q, r, Va
+
+        Returns:
+
+        '''
+        self.attitude_filter.predict(u, t, self.attitudeParams, self.attitudeDynParams)
+        xhat = self.attitude_filter.x_hat
+        P = self.attitude_filter.P
+        return xhat, P
+    
+    def measurement_attitude_accel_update(self, y: np.ndarray, u: np.ndarray, t) -> tuple[np.ndarray, np.ndarray]:
+        '''
+        Args:
+            y: phi, theta
+            u: p, q, r, Va
+            h:
+            R:
+            gate_threshold:
+        '''
+        self.attitude_filter.update(y, u, t, self.attitudeParams, self.accelMeasParams)
+        xhat = self.attitude_filter.x_hat
+        P = self.attitude_filter.P
+        return xhat, P
+    
+    def propagate_position_model(self, u: np.ndarray, t) -> tuple[np.ndarray, np.ndarray]:
+        '''
+        
+        '''
+        self.position_filter.predict(u, t, self.positionParams, self.positionDynParams)
+        xhat = self.position_filter.x_hat
+        P = self.position_filter.P
+        return xhat, P
+    
+    def measurement_position_pseudo_update(self, y: np.ndarray, u: np.ndarray, t) -> tuple[np.ndarray, np.ndarray]:
+        '''
+        
+        '''
+        self.position_filter.update(y, u, t, self.positionParams, self.pseudoMeasParams)
+        xhat = self.position_filter.x_hat
+        P = self.position_filter.P
+        return xhat, P
+    
+    def measurement_position_gps_update(self, y: np.ndarray, u: np.ndarray, t) -> tuple[np.ndarray, np.ndarray]:
+        '''
+        
+        '''
+        self.position_filter.update(y, u, t, self.positionParams, self.gpsMeasParams)
+        xhat = self.position_filter.x_hat
+        P = self.position_filter.P
+        return xhat, P
---
+++ mavsim_python/parameters/estimation_pararameters_ukf.py
@@ -0,0 +1,76 @@
+import numpy as np
+from scipy import stats
+import parameters.sensor_parameters as SENSOR
+import parameters.aerosonde_parameters as MAV
+
+# -------- EKF parameters for observer --------
+Q_attitude=np.diag([
+    (10e-10)**2, # phi
+    (10e-10)**2, # theta
+    ])
+P0_attitude= np.diag([
+    (.01)**2, # phi
+    (.01)**2, # theta
+    ])
+xhat0_attitude=np.array([
+    [MAV.phi0], # phi 
+    [MAV.theta0], # theta
+    ])
+Qu_attitude=np.diag([
+    SENSOR.gyro_sigma**2, 
+    SENSOR.gyro_sigma**2, 
+    SENSOR.gyro_sigma**2, 
+    SENSOR.abs_pres_sigma])
+
+Q_position=np.diag([
+    (10e-2)**2,  # pn
+    (10e-2)**2,  # pe
+    (10e-4)**2,  # Vg
+    (10e-4)**2, # chi
+    (10e-4)**2, # wn
+    (10e-4)**2, # we
+    (10e-4)**2, # psi
+    ])
+P0_position=np.diag([
+    (.01)**2, # pn
+    (.01)**2, # pe
+    (.01)**2, # Vg
+    np.radians(.01)**2, # chi
+    (.01)**2, # wn
+    (.01)**2, # we
+    np.radians(.01)**2, # psi
+    ])
+xhat0_position=np.array([
+    [MAV.north0], # pn 
+    [MAV.east0], # pe 
+    [np.linalg.norm(np.array([[MAV.u0, MAV.v0, MAV.w0]]))], # Vg 
+    [np.arctan2(MAV.v0, MAV.u0)], # chi
+    [0.0], # wn 
+    [0.0], # we 
+    [MAV.psi0], # psi
+    ])
+Qu_position=0.*np.diag([
+    SENSOR.gyro_sigma**2, 
+    SENSOR.gyro_sigma**2, 
+    SENSOR.abs_pres_sigma,
+    np.radians(3), # guess for noise on roll
+    np.radians(3), # guess for noise on pitch
+    ])
+
+R_accel = np.diag([
+        SENSOR.accel_sigma**2, 
+        SENSOR.accel_sigma**2, 
+        SENSOR.accel_sigma**2
+        ])
+accel_gate_threshold = stats.chi2.isf(q=1e-5, df=3)
+R_pseudo = np.diag([
+        0.01,  # pseudo measurement #1 ##### TODO #####
+        0.01,  # pseudo measurement #2 ##### TODO #####
+        ])
+pseudo_gate_threshold = stats.chi2.isf(q=1e-5, df=2)
+R_gps = np.diag([
+        SENSOR.gps_n_sigma**2,  # y_gps_n
+        SENSOR.gps_e_sigma**2,  # y_gps_e
+        SENSOR.gps_Vg_sigma**2,  # y_gps_Vg
+        SENSOR.gps_course_sigma**2,  # y_gps_course
+        ])
